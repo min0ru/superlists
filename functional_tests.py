af1acad43177cdab1fae27ff042d3fa69924967c
min0ru <2010min0ru@gmail.com>
2019-06-30 14:45:18 +0300

    FT DRY Refactoring

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,20 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(
+            row_text,
+            [row.text for row in rows]
+        )
+
+    def post_new_item(self, item_text):
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys(item_text)
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # User opens to-do app
         self.browser.get('http://localhost:8000/')
@@ -30,42 +44,20 @@ class NewVisitorTest(unittest.TestCase):
 
         # He types "Buy peacock feathers" into a text box
         todo_1_text = 'Buy peacock feathers'
-        inputbox.send_keys(todo_1_text)
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        self.post_new_item(todo_1_text)
 
         # When he hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(
-            f'1: {todo_1_text}',
-            (row.text for row in rows),
-            'First to-do item did not appear in table'
-        )
+        self.check_for_row_in_list_table(f'1: {todo_1_text}')
 
         # There is still a text box inviting him to add another item
         # He types "Use peacock feathers to make a fly"
         todo_2_text = 'Use peacock feathers to make a fly'
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys(todo_2_text)
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        self.post_new_item(todo_2_text)
 
         # The page updates again and now shows both items in list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(
-            f'1: {todo_1_text}',
-            (row.text for row in rows),
-            'First to-do item did not appear in table after second post'
-        )
-        self.assertIn(
-            f'2: {todo_2_text}',
-            (row.text for row in rows),
-            'Second to-do item did not appear in table after second post'
-        )
+        self.check_for_row_in_list_table(f'1: {todo_1_text}')
+        self.check_for_row_in_list_table(f'2: {todo_2_text}')
 
         # User checks whether the site will remember his list
         # There is a text bar that has a unique URL for created to-do list with some explanatory text
