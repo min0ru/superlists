c40ada275e00401bea072d1c5bf1fb7499338769
min0ru <2010min0ru@gmail.com>
2019-06-30 14:17:52 +0300

    User input processing and displaying to-do item

--- lists/tests.py
+++ lists/tests.py
@@ -6,3 +6,13 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        test_item_text = 'A new list item'
+        response = self.client.post('/', data={'item_text': test_item_text})
+        self.assertIn(
+            test_item_text,
+            response.content.decode(),
+            'Response should contain just posted data'
+        )
+        self.assertTemplateUsed(response, 'home.html')
--- lists/views.py
+++ lists/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, 'home.html')
+    context = {}
+    if request.method == 'POST':
+        new_item_text = request.POST.get('item_text', '')
+        context['new_item_text'] = new_item_text
+    return render(request, 'home.html', context)
