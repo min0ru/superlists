66eed5e7216b7452f5cf35be871198aac87a6e4d
min0ru <2010min0ru@gmail.com>
2019-07-01 22:46:58 +0300

    FT integrated with django test framework

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,10 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-import unittest
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -28,7 +28,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # User opens to-do app
-        self.browser.get('http://localhost:8000/')
+        self.browser.get(self.live_server_url)
 
         # He notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -66,7 +66,3 @@ class NewVisitorTest(unittest.TestCase):
 
         # FT is unfinished yet
         self.fail('FT should fail here because it\'s not finished yet!')
-
-
-if __name__ == '__main__':
-    unittest.main()
