ec8a44b10980b4508a412532a03b3b3c559c502e
min0ru <2010min0ru@gmail.com>
2019-07-01 23:03:53 +0300

    Adaptive explicit waits in FT

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,7 +1,10 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import time
+
+MAX_WAIT_SECONDS = 10
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -12,19 +15,26 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(
-            row_text,
-            [row.text for row in rows]
-        )
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(
+                    row_text,
+                    [row.text for row in rows]
+                )
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT_SECONDS:
+                    raise e
+                time.sleep(0.1)
 
     def post_new_item(self, item_text):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys(item_text)
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # User opens to-do app
@@ -48,7 +58,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         # When he hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
-        self.check_for_row_in_list_table(f'1: {todo_1_text}')
+        self.wait_for_row_in_list_table(f'1: {todo_1_text}')
 
         # There is still a text box inviting him to add another item
         # He types "Use peacock feathers to make a fly"
@@ -56,8 +66,8 @@ class NewVisitorTest(LiveServerTestCase):
         self.post_new_item(todo_2_text)
 
         # The page updates again and now shows both items in list
-        self.check_for_row_in_list_table(f'1: {todo_1_text}')
-        self.check_for_row_in_list_table(f'2: {todo_2_text}')
+        self.wait_for_row_in_list_table(f'1: {todo_1_text}')
+        self.wait_for_row_in_list_table(f'2: {todo_2_text}')
 
         # User checks whether the site will remember his list
         # There is a text bar that has a unique URL for created to-do list with some explanatory text
