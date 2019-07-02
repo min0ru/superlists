06c619efe4f627b025d82817d9b69977d47d274e
min0ru <2010min0ru@gmail.com>
2019-07-03 00:28:13 +0300

    Refactored duplications in urls.py

--- lists/urls.py
+++ lists/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
+from django.conf.urls import url
 
-from lists import views as lists_views
-from lists import urls as lists_urls
+from lists import views
 
 urlpatterns = [
-    url(r'^$', lists_views.home_page, name='home'),
-    url(r'lists/', include(lists_urls)),
+    url(r'^new$', views.new_list, name='new_list'),
+    url(r'^(\d+)/$', views.view_list, name='view_list'),
+    url(r'^(\d+)/add_item$', views.add_item, name='view_list'),
 ]
